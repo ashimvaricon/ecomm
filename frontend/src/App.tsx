9a17176b20eebbe0a738529c8dbdb211b47dd349
adars124 <lonleyau@gmail.com>
2024-09-12 15:50:07 +0545

    sync with main branch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Routes from "./routes";
 import "./index.css";
-import Products from "./pages/Products";
 
 const App: React.FC = () => {
-  return (
-    <>
-      <Routes />
-    </>
-  );
+    return (
+        <>
+            <Routes />
+        </>
+    );
 };
 
 export default App;
