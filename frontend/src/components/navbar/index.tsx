4c7aaebe22608524cce29e7202eebb87c8f078d6
ashimpoudyal <ashim1998.poudyal@gmail.com>
2024-09-12 14:15:46 +0545

    feat(applayout): Added the applayout

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -2,7 +2,6 @@ import { Box, IconButton, Typography } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
-import { Outlet } from "react-router-dom";
 import NavbarLink, { NavbarLinkProps } from "./NavbarLink";
 
 const navbarItem: NavbarLinkProps[] = [
@@ -40,9 +39,6 @@ const Navbar: React.FC = () => {
           </Box>
         </Toolbar>
       </AppBar>
-      <main style={{ marginTop: "64px" }}>
-        <Outlet />
-      </main>
     </Box>
   );
 };
