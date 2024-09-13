587f86d97a75d19cf62e992e8b1a257416779405
ashimpoudyal <ashim1998.poudyal@gmail.com>
2024-09-13 14:27:38 +0545

    Added Carousel and Image

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,31 @@
-import { Box, Typography } from "@mui/material";
-
+import { Box } from "@mui/material";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slide2 from "../../assets/images/slideBanner2.jpg";
+import { CenteredBox, CustomBox, ImageBox, StyledImage } from "./HomeStyled";
 const Home = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+  };
+
   return (
-    <Box>
-      <Typography variant="h1">Home</Typography>
+    <Box sx={{ height: "50vh", backgroundColor: "grey", marginTop: "70px" }}>
+      <Slider {...settings}>
+        <CenteredBox>
+          <StyledImage src={Slide2} alt="Slide 1" />
+        </CenteredBox>
+        <CustomBox>
+          <StyledImage src={Slide2} alt="Slide 2" />
+        </CustomBox>
+      </Slider>
     </Box>
   );
 };
