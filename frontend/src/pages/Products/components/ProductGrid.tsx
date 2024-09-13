c59c244ee94bbd43293da6052f52f902887ebeb2
adars124 <lonleyau@gmail.com>
2024-09-13 11:05:15 +0545

    refactor: use new grid and decompose card component

--- frontend/src/pages/Products/components/ProductGrid.tsx
+++ frontend/src/pages/Products/components/ProductGrid.tsx
@@ -1,42 +1,20 @@
-import { Typography, Grid, Card, CardContent, CardMedia } from "@mui/material";
-import products from "../../../constants/products";
-import productStyles from "./styles";
+import Grid from "@mui/material/Grid2";
 import { Link } from "react-router-dom";
+import products from "../../../constants/products";
+import ProductCard from "./ProductCard";
 
 const ProductGrid: React.FC = () => {
     return (
         <Grid container spacing={4}>
             {products.map((product, index) => (
-                <Grid item key={index} xs={12} sm={6} md={4}>
+                <Grid key={index} size={{ xs: 2, sm: 4, md: 4 }}>
                     <Link
-                        to={`/product/${product.name
-                            .toLowerCase()
-                            .replace(" ", "-")}`}
+                        to={`/product/${product.name.replace(" ", "-")}`}
                         style={{
                             textDecoration: "none",
                         }}
                     >
-                        <Card sx={productStyles.cardStyles}>
-                            <CardMedia
-                                component="img"
-                                image={product.image}
-                                alt={product.name}
-                            />
-                            <CardContent
-                                sx={{ flexGrow: 1, textAlign: "center" }}
-                            >
-                                <Typography
-                                    gutterBottom
-                                    variant="h5"
-                                    component="h2"
-                                >
-                                    {product.name}
-                                </Typography>
-                                <Typography variant="h6" color="primary">
-                                    Rs. {product.price}
-                                </Typography>
-                            </CardContent>
-                        </Card>
+                        <ProductCard product={product} />
                     </Link>
                 </Grid>
             ))}
