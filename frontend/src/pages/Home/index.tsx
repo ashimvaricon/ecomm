9f68ba8f355743997cca218c8c923c0be925dbde
ashimpoudyal <ashim1998.poudyal@gmail.com>
2024-09-16 09:43:50 +0545

    Seperated settings

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -3,22 +3,12 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slide2 from "../../assets/images/slideBanner2.jpg";
-import { CenteredBox, CustomBox, ImageBox, StyledImage } from "./HomeStyled";
+import { CenteredBox, CustomBox, StyledImage } from "./HomeStyled";
+import { sliderSettings } from "../../utils/sliderSettings";
 const Home = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-  };
-
   return (
     <Box sx={{ height: "50vh", backgroundColor: "grey", marginTop: "70px" }}>
-      <Slider {...settings}>
+      <Slider {...sliderSettings}>
         <CenteredBox>
           <StyledImage src={Slide2} alt="Slide 1" />
         </CenteredBox>
