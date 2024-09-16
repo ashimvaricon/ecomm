58fff19ea8b38b4765bcffc59623e3a76da21efe
adars124 <lonleyau@gmail.com>
2024-09-16 12:45:04 +0545

    refact: change routes from enum to constant

--- frontend/src/constants/routes.ts
+++ frontend/src/constants/routes.ts
@@ -1,10 +1,10 @@
-export enum ROUTES {
-    Home = "/",
-    Products = "/products",
-    ProductDetails = "/products/:slug",
-    Cart = "/cart",
-    Checkout = "/checkout",
-    Orders = "/orders",
-    Profile = "/profile",
-    NotFound = "*",
-}
+export const ROUTES = {
+  Home: "/",
+  Products: "/products",
+  ProductDetails: "/products/:slug",
+  Cart: "/cart",
+  Checkout: "/checkout",
+  Orders: "/orders",
+  Profile: "/profile",
+  NotFound: "*",
+};
--- frontend/src/routes/routeConfig.ts
+++ frontend/src/routes/routeConfig.ts
@@ -2,18 +2,18 @@ import { ROUTES } from "../constants/routes";
 import React from "react";
 
 export interface RouteConfig<T = object> {
-    path: ROUTES;
-    component: React.LazyExoticComponent<React.ComponentType<T>>;
+  path: string;
+  component: React.LazyExoticComponent<React.ComponentType<T>>;
 }
 
 export const routeConfigs: RouteConfig[] = [
-    { path: ROUTES.Home, component: React.lazy(() => import("../pages/Home")) },
-    {
-        path: ROUTES.Products,
-        component: React.lazy(() => import("../pages/Products")),
-    },
-    {
-        path: ROUTES.ProductDetails,
-        component: React.lazy(() => import("../pages/Products/ProductDetails")),
-    },
+  { path: ROUTES.Home, component: React.lazy(() => import("../pages/Home")) },
+  {
+    path: ROUTES.Products,
+    component: React.lazy(() => import("../pages/Products")),
+  },
+  {
+    path: ROUTES.ProductDetails,
+    component: React.lazy(() => import("../pages/Products/ProductDetails")),
+  },
 ];
