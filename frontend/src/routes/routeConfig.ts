97c73ebc1b5493124fe9b0d8bf9d5501c5acd6f9
adars124 <lonleyau@gmail.com>
2024-09-17 16:13:12 +0545

    chore: add cart route in config

--- frontend/src/routes/routeConfig.ts
+++ frontend/src/routes/routeConfig.ts
@@ -6,9 +6,18 @@ export interface RouteConfig<T = object> {
   component: React.LazyExoticComponent<React.ComponentType<T>>;
 }
 
-
 export const routeConfigs: RouteConfig[] = [
   { path: ROUTES.Home, component: React.lazy(() => import("../pages/Home")) },
-  { path: ROUTES.Products, component: React.lazy(() => import("../pages/Products")) },
- 
+  {
+    path: ROUTES.Products,
+    component: React.lazy(() => import("../pages/Products")),
+  },
+  {
+    path: ROUTES.ProductDetails,
+    component: React.lazy(() => import("../pages/Products/ProductDetails")),
+  },
+  {
+    path: ROUTES.Cart,
+    component: React.lazy(() => import("../pages/Cart")),
+  },
 ];
